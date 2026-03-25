14c28baa022353746188cbfefd8b1b3974eb9ee0
Ruslan <crosssimply@gmail.com>
2026-03-25 23:47:30 +0200

    Woking on Domain entities & ICustomerRepositor interface

---
+++ Sales.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Sales.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> GetByIdAsync(int id);
+
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+
+        Task AddAsync(Customer customer);
+    }
+}
